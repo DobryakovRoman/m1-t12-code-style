95b52c09e67924038be3fc5b51cfacbcb4645efc
DobryakovRoman <griffinsc@ya.ru>
2022-07-06 18:18:48 +0300

    0.0.3 style: добавлены пустые строки, переименована переменная outDoubleVar

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -4,14 +4,14 @@ public class DepositCalculator {
 
     double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
-       return random(pay, 2);
+       return round(pay, 2);
     }
 
     double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
-        return random(amount + amount * yearRate * depositPeriod, 2);
+        return round(amount + amount * yearRate * depositPeriod, 2);
     }
-//random - не имеет формы глагола, а это метод. В названии лучше указать глагол) (напишу в лс)
-    double random(double value, int places) { 
+
+    double round(double value, int places) {
         double scale = Math.pow(10, places);
         return Math.round(value * scale) / scale;
     }
@@ -19,22 +19,26 @@ public class DepositCalculator {
     void calculateDeposit() {
         int period;
         int action;
+        int amount;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите сумму вклада в рублях:") ;
-        int amount = scanner.nextInt();  //эта переменная выделяется. Лучше отправить ее к остальным на строку 22. Либо переменные на строках 20-21 привести к такому же виду. 
+        amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:") ;
         period = scanner.nextInt( );
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        action = scanner.nextInt(); //Кажется, что после строки 29 не хватает пробела, чтобы выделить кусок кода с другой логикой. И после строки 37.
-        double outDoubleVar = 0; //DoubleVar - лишнее в названии, т.к. из мы это можем понять из типа переменной, что это переменная и она типа дабл. Но лучше указать, что это сумма.
+        action = scanner.nextInt();
+
+        double percentAmount = 0;
         if (action == 1) {
-            outDoubleVar = calculateSimplePercent(amount, 0.06, period);
+            percentAmount = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
-            outDoubleVar = calculateComplexPercent(amount, 0.06, period);
+            percentAmount = calculateComplexPercent(amount, 0.06, period);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+        
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + percentAmount);
     }
+
     public static void main(String[] args) {
         new DepositCalculator().calculateDeposit();
     }
